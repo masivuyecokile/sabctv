43a3ac4df7d8e0df7ed17645ad37f7eabee43425
masivuyecokile <masivuyecokile@gmail.com>
2018-06-07 11:49:11 +0200

    Create conn.php

---
+++ conn.php
@@ -0,0 +1,56 @@
+<?php
+    
+
+    // require_once(ABSPATH . 'wp-config.php');
+
+define('DB_NAME', 'testpqkbye_db1');
+
+/** MySQL database username */
+define('DB_USER', 'testpqkbye_1');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'P3HhmMRr1A8');
+
+/** MySQL hostname */
+define('DB_HOST', 'dedi835.jnb1.host-h.net');
+
+	class conn
+{
+    protected static $instance = null;
+
+    protected function __construct() {}
+    protected function __clone() {}
+
+    public static function instance()
+    {
+        if (self::$instance === null)
+        {
+            $opt  = array(
+                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES   => FALSE,
+            );
+            $dsn = 'mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset=utf8';
+            self::$instance = new PDO($dsn, DB_USER, DB_PASSWORD, $opt);
+
+
+        }
+        return self::$instance;
+    }
+
+    public static function __callStatic($method, $args)
+    {
+        return call_user_func_array(array(self::instance(), $method), $args);
+    }
+
+    public static function run($sql, $args = [])
+    {
+        if (!$args)
+        {
+             return self::instance()->query($sql);
+        }
+        $stmt = self::instance()->prepare($sql);
+        $stmt->execute($args);
+        return $stmt;
+    }
+}
